4f03561d87cdebfae6f8430d45725ff7156adbed
skam4ik <GrMax000@gmail.com>
2025-04-07 19:00:02 +0300

    mcproject-js

--- papkajs/script.js
+++ papkajs/script.js
@@ -1,4 +1,4 @@
-// З
+// Завдання N1
 function checkindYear (year) {
     if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
         resultFirstTusk.textContent = "Ви народились у високосний рік";
@@ -31,3 +31,64 @@ checkYearButton.addEventListener("click", () => {
         resultFirstTusk.textContent = "Ви народились не у високосний рік";
     }
 })
+
+
+
+// Завдання N2
+
+
+
+
+// Завдання N3
+let currentOperator = '+';
+const plusbutton = document.getElementById('plusbutton');
+const minusbutton = document.getElementById('minusbutton');
+const mogennyabutton = document.getElementById('mogennyabutton');
+const dilennyabutton = document.getElementById('dilennyabutton');
+const clickbutton = document.getElementById('clickbutton');
+plusbutton.addEventListener('click',()=> setOperator('+'));
+minusbutton.addEventListener('click',()=> setOperator('-'));
+mogennyabutton.addEventListener('click',()=> setOperator('*'));
+dilennyabutton.addEventListener('click',()=> setOperator('/'));
+clickbutton.addEventListener('click', calculate);
+function setOperator(op) {
+  currentOperator = op;
+  console.log(op)
+  console.log(currentOperator)
+}
+function calculate() {
+  const num1 = parseFloat(document.getElementById('num1').value);
+  const num2 = parseFloat(document.getElementById('num2').value);
+  let result = '';
+  if (isNaN(num1) || isNaN(num2)) {
+    result = 'Помилка';
+  } else {
+    switch (currentOperator) {
+      case '+':
+        result = num1 + num2;
+        break; 
+      case '-':
+        result = num1 - num2;
+        break;
+      case '*':
+        result = num1 * num2;
+        break;
+      case '/':
+        result = num2 !== 0 ? num1 / num2 : 'Ділення на 0 не можливо виконати';
+        break;
+    }
+  }
+  console.log(document.getElementById('result').value)
+  document.getElementById('result').value = result;
+  console.log(result)
+}
+
+
+
+
+// Завдання N4
+// Завдання N5
+// Завдання N6
+// Завдання N7
+// Завдання N8
+// Завдання N9
